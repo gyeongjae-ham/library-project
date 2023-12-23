722f01eefa275a812b182591bb84b0e9ed57e538
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-12-23 21:09:58 +0900

    #6 - java User to kotlin User migrate
    java User to kotlin User migrate

--- src/test/kotlin/com/group/libraryapp/service/user/JavaUserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/JavaUserServiceTest.kt
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class UserServiceTest @Autowired constructor(
+class JavaUserServiceTest @Autowired constructor(
     private val userRepository: UserRepository,
     private val userService: UserService
 ) {
@@ -42,10 +42,12 @@ class UserServiceTest @Autowired constructor(
     @DisplayName("유저 업데이트가 정상 동작한다")
     fun getUsersTest() {
         // given
-        userRepository.saveAll(listOf(
-            User("A", 20),
-            User("B", null),
-        ))
+        userRepository.saveAll(
+            listOf(
+                User("A", 20),
+                User("B", null),
+            )
+        )
 
         // when
         val results = userService.getUsers()
@@ -61,7 +63,7 @@ class UserServiceTest @Autowired constructor(
     fun updateUserNameTest() {
         // given
         val savedUser = userRepository.save(User("A", null))
-        val request = UserUpdateRequest(savedUser.id, "B")
+        val request = UserUpdateRequest(savedUser.id!!, "B")
 
         // when
         userService.updateUserName(request)
