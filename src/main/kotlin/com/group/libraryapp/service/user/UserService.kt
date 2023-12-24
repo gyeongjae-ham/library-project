3e8591353b5bb68ac98ebd3fed32640a2079dc25
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-12-24 16:12:27 +0900

    #10 - BookService class java to kotlin 마이그레이션
    kotlin으로 BookService를 마이그레이션 했다. kotlin의 장점을 살려서 Optional을 제거했다
    1. null인 경우 엘비스 표현(?:)으로 에러 컨트롤했고, 해당 부분이 중복되는 걸 함수로 표현해서 간략하게 표현했다.
    2. 기본 jpaRepository 함수의 경우 Optional을 제거할 수 없기 때문에 kotlin과의 호환성을 고려해 만든 findByIdOrNull 메소드를 사용해서 똑같이 엘비스 표현으로 표현했다.
    3. 한 단계 더 나아가 확장함수를 사용해서 더 간략히 표현했다

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -5,6 +5,8 @@ import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.util.fail
+import com.group.libraryapp.util.findByIdOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -27,13 +29,18 @@ class UserService(
 
     @Transactional
     fun updateUserName(request: UserUpdateRequest) {
-        val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        // 기존 crudRepository로는 optional 형태를 내가 제어할 수 없지만
+        // val user = userRepository.findById(request.id).orElseThrow(::IllegalArgumentException)
+        // kotlin과의 사용성을 고려해 만들어둔 findByIdOrNull을 사용하면 똑같이 사용할 수 있다
+        // val user = userRepository.findByIdOrNull(request.id) ?: fail()
+        // 하지만 여기서 한번더 kotlin의 확장함수를 만들어서 더 간단하게 표현할 수 있다
+        val user = userRepository.findByIdOrThrow(request.id)
         user.updateName(request.name)
     }
 
     @Transactional
     fun deleteUser(name: String) {
-        val user = userRepository.findByName(name).orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByName(name) ?: fail()
         userRepository.delete(user)
     }
 
